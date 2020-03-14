8970a6c39982258d5bb6e62094c3b8102de9d05f
Clay Sampson <pdgeek@gmail.com>
2020-03-14 13:36:22 -0400

    first cut at webserver

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <signal.h>
+#include <libgen.h>
 #include <string>
 #include "utils/debug.hpp"
+#include "utils/string_utils.hpp"
+#include "mongoose.h"
 
 struct Options {
   std::string ui_dir; //!< Path to where the UI lives
   std::string web_port; //!< Port for the web server, defaults to 80
   std::string web_addr; //!< Bind address for web server, defaults to 0.0.0.0 (any address)
+  bool error;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+// Stop when this is false
+////////////////////////////////////////////////////////////////////////////////
+static bool run = true;
+
+////////////////////////////////////////////////////////////////////////////////
+// on_sigint
+////////////////////////////////////////////////////////////////////////////////
+static void on_sigint(int signal) {
+  (void)signal; // not used
+  if(!run) { exit(-1); }
+  run = false;
+} // end on_sigint
+
+////////////////////////////////////////////////////////////////////////////////
+// on_sigterm
+////////////////////////////////////////////////////////////////////////////////
+static void on_sigterm(int signal) {
+  (void)signal; // not used
+  if(!run) { exit(-1); }
+  run = false;
+} // end on_sigterm
+
+
+////////////////////////////////////////////////////////////////////////////////
+// setup_signals
+////////////////////////////////////////////////////////////////////////////////
+static void setup_signals(void) {
+  struct sigaction action;
+
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = on_sigint;
+  sigaction(SIGINT, &action, nullptr);
+
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = on_sigterm;
+  sigaction(SIGTERM, &action, nullptr);
+
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = SIG_IGN;
+  sigaction(SIGPIPE, &action, nullptr); // retarded bsd behavior from way back is still inconsistent today
+
+} // end setup_signals
 
 ////////////////////////////////////////////////////////////////////////////////
 // parse_command_line
 ////////////////////////////////////////////////////////////////////////////////
 static Options parse_command_line(int argc, char* argv[]) {
+  std::shared_ptr<char> prog = util_strdup(argv[0]);
   Options o;
-  o.ui_dir =
+  o.error = false;
+  o.ui_dir = dirname(prog.get());
   o.web_port = "80";
   o.web_addr = "0.0.0.0";
 
@@ -57,24 +108,67 @@ static Options parse_command_line(int argc, char* argv[]) {
 
       case '?':
       /* getopt_long already printed an error message. */
+      o.error = true;
       break;
 
       default:
-        abort();
+      o.error = true;
+      break;
       } // end switch option
   } // end while
   return o;
 } // end parse_command_line
 
 ////////////////////////////////////////////////////////////////////////////////
+// ev_handler
+////////////////////////////////////////////////////////////////////////////////
+static void ev_handler(struct mg_connection *c, int ev, void *p) {
+  if(ev != MG_EV_HTTP_REQUEST) {
+    return;
+  }
+
+  struct http_message *hm = (struct http_message *) p;
+  c->user_data = (void*)0xDEADBEEF;
+  iodpr(true, hm->message.p, hm->message.len);
+  dpr("method        : %.*s\n", int(hm->method.len), hm->method.p);
+  dpr("uri           : %.*s\n", int(hm->uri.len), hm->uri.p);
+  dpr("proto         : %.*s\n", int(hm->proto.len), hm->proto.p);
+  dpr("query_string  : %.*s\n", int(hm->query_string.len), hm->query_string.p);
+  dpr("user_data     : %p\n", c->user_data);
+
+  // We have received an HTTP request. Parsed request is contained in `hm`.
+  // Send HTTP reply to the client which shows full original request.
+  mg_send_head(c, 200, hm->message.len, "Content-Type: text/plain");
+  mg_printf(c, "%.*s", (int)hm->message.len, hm->message.p);
+} // end ev_handler
+////////////////////////////////////////////////////////////////////////////////
 // main
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
-  Options o = parse_command_line(argc, argv);
+  dpr("Startup\n");
+  setup_signals();
 
+  Options o = parse_command_line(argc, argv);
+  if(o.error) {
+    fprintf(stderr, "Error parsing options\n");
+    return EXIT_FAILURE;
+  }
   dpr("ui_dir = %s\n", o.ui_dir.c_str());
   dpr("web_port = %s\n", o.web_port.c_str());
   dpr("web_addr = %s\n", o.web_addr.c_str());
 
+  struct mg_mgr mgr;
+  struct mg_connection *c;
+
+  mg_mgr_init(&mgr, NULL);
+  c = mg_bind(&mgr, "8080", ev_handler);
+  mg_set_protocol_http_websocket(c);
+
+  while(run) {
+    mg_mgr_poll(&mgr, 1000);
+  }
+  mg_mgr_free(&mgr);
+
+  dpr("Cleanup\n");
   return EXIT_SUCCESS;
 } // end main
