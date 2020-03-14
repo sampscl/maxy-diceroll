3cc9a608001f4e54efe5fedd53619280fda1561f
Clay Sampson <pdgeek@gmail.com>
2020-03-14 12:49:47 -0400

    initial skeleton

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  maxy-diceroll
+//
+//  Created by Clay Sammpson on 3/14/2020.
+//  Copyright 3/14/2020 Clay Sammpson. All rights reserved.
+//
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  return EXIT_SUCCESS;
+} // end main
